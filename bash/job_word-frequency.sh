136f783138d6f10034555a2b302ab0e7d47db449
Carol Booth <67783469+carolbooth2@users.noreply.github.com>
2025-11-18 08:55:56 +0000

    Add job script to call word-freq.sh

---
+++ bash/job_word-frequency.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --partition=short
+#SBATCH --job-name=data_sort_single
+#SBATCH --nodes=1
+#SBATCH --tasks=1
+#SBATCH --cpus-per-task=1
+
+echo "Starting word frequency script"
+bash word-freq.sh
+echo "Finished word frequency script"
